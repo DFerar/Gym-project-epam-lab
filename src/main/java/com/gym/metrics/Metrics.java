cd9a7b418a3516ec718e73d94f13c6e6fd1d6ea0
DFerar <mr.ferar@gmail.com>
2024-06-20 18:55:01 +0400

    wip

--- src/main/java/com/gym/metrics/Metrics.java
+++ src/main/java/com/gym/metrics/Metrics.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Metrics {
     private final Counter requestCounter;
-    private final Gauge memoryGauge;
+    private final Gauge memoryGauge; //TODO: cancel
 
     /**
      * The Metrics constructor. Initializes a Counter and a Gauge with the provided MeterRegistry.
